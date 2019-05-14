7d13e2eeb30dd858df58180e8943f4bb7ddcf154
jonathan martinez <jony-5@Jony-5.ksd.org>
2019-05-14 12:40:47 -0700

    AA

--- myneo.c
+++ myneo.c
@@ -3,35 +3,35 @@ int r,c;//row and column
 int red = 0, green = 0, blue = 0;
 // add your array
 int m[8][8] = {
-      {0,1,2,3,4,5,0,1} ,
-      {2,3,4,5,0,1,2,3} ,
-      {4,5,0,1,2,3,4,5} ,
-      {0,1,2,3,4,5,0,1} ,
-      {2,3,4,5,0,1,2,3} ,
-      {4,5,0,1,2,3,4,5} ,
-      {0,1,2,3,4,5,0,1},
-      {2,3,4,5,0,1,2,3}
-    };
+      {0,0,0,0,0,0,0,0} ,
+      {0,0,0,0,0,0,0,0} ,
+      {0,0,0,0,0,0,0,0} ,
+      {0,0,0,0,0,0,0,0} ,
+      {0,0,0,0,0,0,0,0} ,
+      {0,0,0,0,0,0,0,0} ,
+      {0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0}
+    };A
   i = 0;// set neo-pixel number to zero
   int val;
-//  loop that increments x,y and i
+//  loop thAat increments x,y and i
   for (r = 0; r < 8; r++){
     for (c = 0; c < 8; c++){
       // set the color for red green and blue
-      val = m[r][c];
+      val =A m[r][c];brown color rgb
       // add your switch statement
       switch ( val ) {
-          case 0:
+          cAase 0:
                 red = 100; green = 0; blue =0;//red
           break;
           case 1:
                 red = 0; green = 100; blue =0; //green
           break;
-           case 2:
-              red = 0; green = 0; blue =100; //blue
+           case 2:A
+          A    red = 0; green = 0; blue =100; //blue
           break;
             case 3:
-              red = 100; green = 100; blue =0; //yellow
+              red = 130; green = 82; blue = 1; //brown
           break;
             case 4:
               red = 0; green = 50; blue = 50; //cyan
---
+++ pointers3.cpp
@@ -0,0 +1,35 @@
+// JM pointers3.cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+  int integers[10];
+  int * pointer;
+  pointer = integers;
+  //print array
+  cout<<"print intergers[] array"<<"\n\n";
+  for (int n = 0; n < 5; n++){
+  //integers[n] = 0;
+  cout << integers[n]<<"\n";
+  }
+//Increment *pointer then assign a value
+  *pointer = 11;
+  pointer++; *pointer = 22;
+  pointer++; *pointer = 33;
+  pointer++; *pointer = 44;
+  pointer++; *pointer = 55;
+  pointer=pointer +2; *pointer = 77;
+  //print array memory locations
+  cout<<"\nprint &interfers[] memory address "<<"\n\n";
+  for (int n = 0; n < 10; n++){
+  cout << &integers[n]<<"\n";
+  cout<<endl;
+  }
+  //print array memory locations
+  cout<<"\nprint &interfers[] memory address "<<"\n\n";
+  for (int n = 0; n < 10; n++){
+  cout << &integers[n]<<"\n";
+  cout<<endl;
+}
+  return 0;
+}
